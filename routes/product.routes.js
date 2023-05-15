7bcb01961c27e63970e208a9bb9cc793b929a79f
Lorena Lario <lorenalariotudela1999@gmail.com>
2023-05-15 18:08:07 +0200

    purchases implementados

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,61 +1,66 @@
-const express = require('express'); 
-const router = express.Router();  
+const express = require("express");
+const router = express.Router();
 
-const Product = require("../models/Product.model.js")
-const Purchase = require("../models/Purchase.model.js")
-
+const Product = require("../models/Product.model.js");
+const Purchase = require("../models/Purchase.model.js");
 
 //* GET /product/products => renderiza todos los productos
 
-router.get('/products', (req, res, next) => {
-    Product.find()
-    .then(products => {
-        res.render('products/products.hbs', {products: products})
-      })
-    .catch(err => next(err))
-})
+router.get("/products", (req, res, next) => {
+  Product.find()
+    .then((products) => {
+      res.render("products/products.hbs", { products: products });
+    })
+    .catch((err) => next(err));
+});
 
 //* GET /product/products/:id => renderiza un producto por su id
 
-router.get('/products/:id', (req, res, next) => {
-    Product.findById(req.params.id)
-   .then(products => {
-        res.render('products/product-details.hbs', {products: products})
-      })
-   .catch(err => next(err))
-})
+router.get("/products/:id", (req, res, next) => {
+  Product.findById(req.params.id)
+    .then((products) => {
+      res.render("products/product-details.hbs", { products: products });
+    })
+    .catch((err) => next(err));
+});
 
 //* GET /product/:id/purchase => comprar un producto por su id
 
-router.get('/:id/purchase', (req, res, next) => {
-    Product.findById(req.params.id)
-    .then(product => {
-        res.render('products/purchase.hbs', {product: product})
-      })
-    .catch(err => next(err))  
-})
+router.get("/:id/purchase", (req, res, next) => {
+  Product.findById(req.params.id)
+    .then((product) => {
+      res.render("products/purchase.hbs", { product: product });
+    })
+    .catch((err) => next(err));
+});
 
 //* POST /product/:id/purchase => comprar un producto por su id
 
-router.post('/:id/purchase', (req, res, next) => {
-    Product.findById(req.params.id)
+router.post("/:id/purchase", (req, res, next) => {
+  if (
+    req.body.buyerName === "" ||
+    req.body.shippingAddress === "" ||
+    req.body.paymentMethod === ""
+  ) {
+    res.render("products/purchase.hbs", {
+      errorMessage: "Por favor, rellene todos los campos",
+    });
+    return;
+  }
+  Product.findById(req.params.id)
     .then((response) => {
-        Purchase.create({
+      Purchase.create({
         buyerName: req.body.buyerName,
         shippingAddress: req.body.shippingAddress,
         purchasedProduct: response,
         paymentMethod: req.body.paymentMethod,
-        })
-        
+      });
     })
-    
-  .then(() =>{
-    res.redirect('/profile')
-  })
-  .catch(err => next(err))
 
-})
+    .then(() => {
+      res.redirect("/profile");
+    })
+    .catch((err) => next(err));
+});
 
-
-
-module.exports = router;  
+module.exports = router;
--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,82 +1,92 @@
-const express = require('express'); 
-const router = express.Router();  
+const express = require("express");
+const router = express.Router();
 
-const User = require("../models/User.model.js")
-const Product = require("../models/Product.model.js")
-// const Purchase = require("../models/Purchase.model.js")
+const User = require("../models/User.model.js");
+const Product = require("../models/Product.model.js");
+const Purchase = require("../models/Purchase.model.js");
 
-
-// * RUTAS DE USUARIO 
-
+// * RUTAS DE USUARIO
 
 //* GET "/profile" => Renderiza el perfil de un usuario estándar.
 
-router.get("/", (req, res, next) =>{
-    User.findOne(req.session.activeUser)
+router.get("/", (req, res, next) => {
+  User.findOne(req.session.activeUser)
     .populate("purchasesMade")
-    .then((foundUser)=>{
-        res.render("users-views/dashboard.hbs", {
-            foundUser: foundUser
-        })
+    .then((foundUser) => {
+      res.render("users-views/dashboard.hbs", {
+        foundUser: foundUser,
+      });
     })
     .catch((error) => {
-        next(error)
-    })
-})
-
-
+      next(error);
+    });
+});
 
 //* RUTAS PRIVADA PARA ADMINS
 
 //* GET "/profile/admin" ==> Renderiza la vista privada únicamente para admins
 
-router.get("/admin", (req, res, next)=>{
-    User.findOne(req.session.activeUser)
-    .then((foundUser)=>{
-
-        res.render("users-views/admin-dashboard.hbs", {
-            foundUser: foundUser
-        })
-
-    })
-    .catch((error) => {
-        next(error)
-    })
-})
-
+router.get("/admin", async (req, res, next) => {
+  try {
+    const foundUser = await User.findOne(req.session.activeUser)
+    res.render("users-views/admin-dashboard.hbs", {
+      foundUser: foundUser,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
 //* GET "/profile/add-product" => Renderiza la vista privada de admins para crear nuevos productos y añadirlos a la base de datos.
 
-router.get("/add-product", (req, res, next)=>{
-  
-  res.render("users-views/admin-add-product.hbs")
-
-})
-
-
+router.get("/add-product", (req, res, next) => {
+  res.render("users-views/admin-add-product.hbs");
+});
 
 //* POST "/profile/add-product" => Recibe la información del admin y crea el producto en la base de datos
 
-router.post("/add-product", (req, res, next)=>{
-    Product.create({
-   name: req.body.name,
-   price: req.body.price,
-   category: req.body.category,
-   description: req.body.description,
-   // productImage: req.file.path // todo
+router.post("/add-product", (req, res, next) => {
+  if (
+    req.body.name === "" ||
+    req.body.price === "" ||
+    req.body.category === "" ||
+    req.body.description === ""
+  ) {
+    res.render("users-views/admin-add-product.hbs", {
+      errorMessage: "Por favor, rellene todos los campos",
+    });
+    return;
+  }
+  Product.create({
+    name: req.body.name,
+    price: req.body.price,
+    category: req.body.category,
+    description: req.body.description,
+    // productImage: req.file.path // todo
+  })
+    .then(() => {
+      res.redirect("/profile/admin");
     })
-    .then(()=>{
-        res.redirect("/profile/admin")
-    })
-    .catch((error)=>{
-        console.log(error)
-      })
-    // res.render("users-views/admin-add-product.hbs")
+    .catch((error) => {
+      console.log(error);
+    });
+  // res.render("users-views/admin-add-product.hbs")
+});
 
+//* GET /profile/purchase-history => Renderiza la vista de las compras en la web
+
+router.get("/purchase-history", (req, res, next) => {
+    Purchase.find().populate("purchasedProduct")
+   
+    .then((foundPurchase) => {
+         res.render("users-views/admin-purchase-history.hbs", {
+            foundPurchase: foundPurchase
+         })
+    })
+
+    .catch((error) => {
+        next(error);
+    })
 })
 
-
-
-
-
-module.exports = router;  
+module.exports = router;
