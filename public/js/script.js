8e0ccbc23f56a770108e8b9f2ab94b2fa12aa2f0
Lorena Lario <lorenalariotudela1999@gmail.com>
2023-05-18 16:38:10 +0200

    Mensaje de cookies implementado

--- public/js/script.js
+++ public/js/script.js
@@ -2,3 +2,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("store-app JS imported successfully!");
 });
+
+
+let cookieMessage = document.getElementById('cookie-message');
+
+let cookiesAccepted = localStorage.getItem('cookiesAccepted');
+
+
+if (!cookiesAccepted) {
+  cookieMessage.style.display = 'block';
+} else {
+  cookieMessage.style.display = 'none';
+}
+
+
+function acceptCookies() {
+  localStorage.setItem('cookiesAccepted', 'true');
+  cookieMessage.style.display = 'none';
+}
+
+function rejectCookies() {
+  cookieMessage.style.display = 'none';
+}
